989220432a493b3870f677d49984786f82d52299
Joseph Yoo <joseph_yoo@brown.edu>
2019-04-16 16:56:20 -0400

    add comments to formulas and write tests

--- formulas.js
+++ formulas.js
@@ -1,58 +1,83 @@
-/* Input Parameters */
-var L = random(20,80),
-    v0 = random(3.6, 24.6),
-    alt = random(0, 10000),
-    z = random(20, 200);
+/* Formulas */
 
-// Output
-var v1 = wind_velocity(v0, z),
-		T = temp_at(alt),
-		P = air_pressure(T),
-		S = blade_sweep_area(L),
-		_p_ = air_density(P, T),
-		kilowatts_produced = power(_p_, S, v1);
+/*  v_2 = v_1 * z^n
+*
+* 	v_1 : initial wind velocity
+* 	  z : elevation (20-200m)
+*			n : 0.13 (constant)
+*/
+module.exports.wind_velocity = function(v_1, z) {
+	var n = 0.13;
 
-
-// Wind Speed at Tower Height
-function wind_velocity(v0, z) {
-	var N = 0.13;
-
-	return v0 * Math.pow(z, N);
+	return v_1 * Math.pow(z, n);
 }
 
-// Temperature at altitude
-function temp_at(a) {
+/* 	  T = T_s − (a * z)
+*
+*		T_s : temperature at sea level assume 293°K
+*			a : altitude (0-10000m)
+* 		z : atmospheric lapse rate 0.0065 K/m
+*/
+module.exports.temp_at = function(a) {
 	var T_s = 293,
 			z = 0.0065;
+
 	return T_s - (a * z);
 }
 
-// Air Pressure
-function air_pressure(T_a) {
+/* 	  P = P_s (T/T_s)^{-g/(a*R)}
+*
+*		  g : gravitational acceleration 9.8m/s^2
+*			R : air gas constant 287 J/kgK
+* 		a : atmospheric lapse rate 0.0065 K/m
+*			T : temperature at altitude
+* 	T_s : temperature at sea level
+*   P_s : pressure at sea level 101,300 Pa
+*/
+module.exports.air_pressure = function(T) {
 	var g = 9.8,
 			R = 287,
 			a = 0.0065,
 			T_s = 293,
 			P_s = 101300;
 
-	return P_s * Math.pow(T_a/T_s, 5.25);
+	return P_s * Math.pow(T / T_s, -5.25);
 }
 
-// Blade sweep area
-function blade_sweep_area(L) {
+/* 	  S = PI * L^2
+*
+*			L : blade length (20-80m)
+*/
+module.exports.blade_sweep_area = function(L) {
 	return Math.PI * Math.pow(L, 2);
 }
 
-// Air density
-function air_density(p, T) {
-	return p / (287 * T);
+/* 	_p_ = p/(RT)
+*
+*			p : air pressure
+*			R : air gas constant 287 J/kgK
+* 		T : temperature at altitude
+*/
+module.exports.air_density = function(p, T) {
+	var R = 287;
+
+	return p / (R * T);
 }
 
-// Power
-function power(p, A, v) {
+/* 	  P = (p * A * v^3) / 2
+*
+*			p : air pressure
+*			A : blade sweep area
+* 		v : wind speed (3.6-24.6m/s)
+*/
+module.exports.power = function(p, A, v) {
 	return (p * A * Math.pow(v, 3)) / 2
 }
 
-function random(min, max) {
+
+
+
+// helper function to generate random values in a range
+module.exports.random = function(min, max) {
 	return Math.floor(Math.random() * max) + min;
 }
