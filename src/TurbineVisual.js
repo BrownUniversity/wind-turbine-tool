ca4712a1df295618e14eba215f9b2cbed45777e5
kellyegan <kelly.egan@gmail.com>
2019-05-30 20:50:13 -0400

    Add placeholder layout for visual.

--- src/TurbineVisual.js
+++ src/TurbineVisual.js
@@ -2,7 +2,13 @@ import React from 'react';
 import "./TurbineVisual.css";
 
 function TurbineVisual(props) {
-	return <div className="turbine-visual">Picture here</div>
+	return (
+	<div className="turbine-visual">
+
+
+
+	</div>
+	);
 }
 
 export default TurbineVisual;
